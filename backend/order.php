35b01790ef10880025a350bc872d891418768c06
張 建文 <chang180@gmail.com>
2020-08-12 21:56:30 +0800

    後台訂單管理功能完成，已達及格標準

---
+++ backend/order.php
@@ -0,0 +1,48 @@
+<h1 class="ct">訂單管理</h1>
+<form action="api/fastdel.php" method="post" onsubmit="return confirm('確定？')">
+快速刪除：<input type="radio" name="mode" value="1">
+<input type="text" name="date">依日期
+<input type="radio" name="mode" value="2">依電影<select name="name">
+<?php
+$movies=$Movie->all();
+foreach($movies as $m){
+?>
+<option value="<?=$m['name'];?>"><?=$m['name'];?></option>
+<?php }
+?>
+</select>
+<button>刪除</button>
+</form>
+<form action="api/edit_order.php">
+<table rule="rows">
+    <tr>
+        <td>訂單編號</td>
+        <td>電影名稱</td>
+        <td>日期</td>
+        <td>場次時間</td>
+        <td>訂購數量</td>
+        <td>訂購位置</td>
+        <td>操作</td>
+    </tr>
+    <?php
+$ords=$Ord->all([]," ORDER BY no DESC   ");
+foreach($ords as $o){
+    
+    ?>
+    <tr>
+        <td><?=$o['no'];?></td>
+        <td><?=$o['name'];?></td>
+        <td><?=$o['date'];?></td>
+        <td><?=$sess[$o['session']];?></td>
+        <td><?=$o['qt'];?></td>
+        <td><?php
+        $seats=unserialize($o['seat']);
+        foreach($seats as $s){
+            echo $s,",";
+        }
+        ?></td>
+        <td><a href="api/del_order.php?id=<?=$o['id'];?>"><button type="button">刪除</button></a></td>
+    </tr>
+<?php } ?>
+</table>
+</form>
